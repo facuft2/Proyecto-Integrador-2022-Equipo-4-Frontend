29bac600772af19d10aaa642dfe68cc28b3f5455
mathiassellanes <elmathikapo@hotmail.com>
2022-10-14 08:28:32 -0300

    folders changes

--- src/components/ExchangeRequestRecieved/index.jsx
+++ src/components/ExchangeRequestRecieved/index.jsx
@@ -1,27 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
 
 import HeaderGen from "../../components/HeaderGen";
-import { getExchangeById } from "../../api";
 import ExchangeWindow from "../../components/Exchange";
 import UserInfo from "../../components/UserInfo";
 
 import "./index.scss";
 
-const ExchangeRequest = () => {
+const ExchangeRequestRecieved = ({ exchange }) => {
   const [color, setColor] = useState("");
   const [button, setButton] = useState("");
-  const [exchange, setExchange] = useState();
   const [exchangeState, setExchangeState] = useState();
-  const { id } = useParams();
 
-  useEffect(() => {
-    Promise.all(getExchangeById(id)
-    .then((data) =>
-      setExchange(data)
-    ))
-  }, []) 
-
+  // console.log('recibido')
   useEffect(() => {
     switch (exchange?.estado) {
       case "ESPERANDO":
@@ -48,31 +38,30 @@ const ExchangeRequest = () => {
   return (
     <div className="exchange-request">
       <div className="exchange-request__body">
-      <HeaderGen text="Solicitud de intercambio" />
-      <div className="exchange-request__state">
-        <span style={{color: color}} className="exchange-request__state-status">{exchangeState}</span>
-      </div>
-      <ExchangeWindow myProduct={exchange?.producto_enviado} otherProduct={exchange?.producto_recibido} myProfile={exchange?.producto_enviado?.usuario} otherProfile={exchange?.producto_recibido?.usuario} />
+        <HeaderGen text="Solicitud de intercambio" />
+        <div className="exchange-request__state">
+          <span style={{ color: color }} className="exchange-request__state-status">{exchangeState}</span>
+        </div>
+        <ExchangeWindow myProduct={exchange?.producto_recibido} otherProduct={exchange?.producto_enviado} myProfile={exchange?.producto_recibido?.usuario} otherProfile={exchange?.producto_enviado?.usuario} />
         <div className="exchange-request__body-description">
           <span className="exchange-request__body-description-text">Descripcion de el producto</span>
-          <span className="exchange-request__body-description-info">{exchange?.producto_recibido?.descripcion}</span>
+          <span className="exchange-request__body-description-info">{exchange?.producto_enviado?.descripcion}</span>
         </div>
         <div className="exchange-request__body-user">
-          <span className="exchange-request__body-user-text">Usuario que intercambias</span>
-          <UserInfo profile={exchange?.producto_recibido?.usuario} />
+          <span className="exchange-request__body-user-text">Con quien intercambias?</span>
+          <UserInfo profile={exchange?.producto_enviado?.usuario} />
         </div>
         <div className="exchange-request__body-message">
-          <span className="exchange-request__body-message-text">Mensaje que le enviaste</span>
+          <span className="exchange-request__body-message-text">Mensaje que recibiste</span>
           <span className="exchange-request__body-message-info">{exchange?.mensaje}</span>
         </div>
-        <div className="exchange-request__body-bottom">
-          {
-            button
-          }
+        <div className="exchange-request__body-bottom-recieved">
+          <button className="exchange-request__body-bottom-recieved-button--reject">Rechazar</button>
+          <button className="exchange-request__body-bottom-recieved-button--accept">Aceptar</button>
         </div>
       </div>
     </div>
   );
 }
 
-export default ExchangeRequest;
+export default ExchangeRequestRecieved;
